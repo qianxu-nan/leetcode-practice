4c8b020786f18e636d869a2252eeeab4de0be932
Qian Xu <edmondlinnan@gmail.com>
2021-04-09 17:48:33 -0400

    add Leetcode1347.java

---
+++ Leetcode1347.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Leetcode1347 {
+    public static int minSteps(String s, String t) {
+        int[] s_num= new int[26];
+        int[] t_num=new int[26];
+        for(int i=0;i<s.length();i++){
+            s_num[s.charAt(i)-'a']+=1;
+            t_num[t.charAt(i)-'a']+=1;
+        }
+        int result=0;
+        for(int i=0;i<26;i++){
+            result+=Math.abs(s_num[i]-t_num[i]);
+
+
+        }
+        return result/2;
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        String s1="leetcode";
+        String s2="practice";
+        minSteps(s1,s2);
+        System.out.println(minSteps(s1,s2));
+
+    }
+}
